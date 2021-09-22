88e83f79e6e55f21b7127e5515d005327530ecbf
Joel Alfaro <alfarojoela@outlook.com>
2021-09-21 23:06:48 -0700

    tinkering with readlines()

--- main.py
+++ main.py
@@ -7,9 +7,37 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-letters = open("./Input/Letters/starting_letter.txt", "r")
-print(letters.readlines())
+#letters = open("./Input/Letters/starting_letter.txt", "r")
+#print(letters.readlines())
 
 names = open("./Input/Names/invited_names.txt", "r")
-print(names.readlines())
+#appears that return values of readlines() need to be stored into a variable after assignment.
+#cannot access names.readlines() repeatedly.
+list_of_names = names.readlines()  #returns a list of text from file?  does this only work 1 time after opening?
+print(list_of_names)
+
+#print(names.readlines())  #returns a list of text from file?
+
+#list_of_names = names.readlines()
+#print(list_of_names)
+#print(len(list_of_names))
+
+
+#examples = open("./Output/ReadyToSend/example.txt", "r")
+#print(examples.readlines())
+
+
+#HOW replace() works:
+# to replace text in a string like txt = "one one was a race horse"
+#x = txt.replace("one", "fuck") # first argument finds text to replace, second argument is the replacement.
+# print(x)
+
+#HOW strip() works:
+#txt = "      banana      "
+#x = txt.strip()  #this gets rid of all blank space in the string
+
+
+
+
+
 
