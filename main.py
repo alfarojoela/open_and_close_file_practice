3e850fbd89e6a04572d1d4af883ffbcf65969f15
Joel Alfaro <alfarojoela@outlook.com>
2021-09-22 15:48:03 -0700

    creating several lists through each iteration of editing.  each new list needs to be appended.  does not appear that existing elements can be edited.  everything updated.

--- main.py
+++ main.py
@@ -7,71 +7,40 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-#letters = open("./Input/Letters/starting_letter.txt", "r")
-#print(letters.readlines())
+# f = open("demofile.txt", "r")
+# print(f.readline())
+# f.close()
 
+#names is saved as a class object.
 names = open("./Input/Names/invited_names.txt", "r")
-#appears that return values of readlines() need to be stored into a variable after assignment.
-#cannot access names.readlines() repeatedly.
-#not sure why this is?
-list_of_names = names.readlines()  #returns a list of text from file?  does this only work 1 time after opening?
-#print(list_of_names)
 
-new_list = []
-new_line = chr(10)
-for name in list_of_names:
-    new_list.append(name.replace(new_line, " "))
+#reads every line of names object.  each line becomes an element in the names_list.  however each name will be followed
+#by a newline char.  example: name_list[0] = 'Aang\n'
+names_list_with_newline_char = names.readlines()
 
-#print(list_of_names)
-#print(new_list)
+names_edited = []
 
-list_edited = []
-for name in new_list:
-    list_edited.append(name.strip())
+for index in names_list_with_newline_char:
+    name_without_char = index.replace('\n', '')
+    names_edited.append(name_without_char)
 
-print(list_edited)
+names.close()
 
-file = open("./Input/Letters/starting_letter.txt")
+letter_contents= open("./Input/Letters/starting_letter.txt", "r")
+letter_body = letter_contents.readlines()
+letter_contents.close()
 
-#needs a loop here.  salutation stays the same.  overwrite salutation_and_name through each iteration
-#then take body_of_letter and overwrite subscript 0.  then write and export to separate file.
-#appears body of letter needs to be saved as a list before the loop iterates.
-#also needs list_edited name appended to name of file name to save.
-salutation = file.readline()
-salutation_and_name = salutation.replace("[name]", list_edited[0])
-print(salutation_and_name)
-salutation_and_name = salutation.replace("[name]", list_edited[2])
-print(salutation_and_name)
-#print(names.readlines())  #returns a list of text from file?
-
-
-file = open("./Input/Letters/starting_letter.txt")
-body_of_letter = file.readlines()
-print(body_of_letter)
-
-body_of_letter[0] = salutation_and_name
-
-print(body_of_letter)
-
-#list_of_names = names.readlines()
-#print(list_of_names)
-#print(len(list_of_names))
+finished_letter = []
+for index in range(len(names_edited)):
+    first_line_to_append = letter_body[0].replace('[name]', names_edited[index])
+    #print(first_line_to_append)
+    finished_letter.append(first_line_to_append)
+    for jindex in range(2, len(letter_body)):
+        finished_letter.append(letter_body[jindex])
 
 
 
 
-#examples = open("./Output/ReadyToSend/example.txt", "r")
-#print(examples.readlines())
-
-
-#HOW replace() works:
-# to replace text in a string like txt = "one one was a race horse"
-#x = txt.replace("one", "fuck") # first argument finds text to replace, second argument is the replacement.
-# print(x)
-
-#HOW strip() works:
-#txt = "      banana      "
-#x = txt.strip()  #this gets rid of all blank space in the string
 
 
 
